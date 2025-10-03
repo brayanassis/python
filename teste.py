8e263e9bf5e85a6306f2ae383d33af9cd6b92ced
brayanassis <142243436+brayanassis@users.noreply.github.com>
2025-10-03 11:59:27 -0300

    Add files via upload

---
+++ teste.py
@@ -0,0 +1,18 @@
+print("Meros humanos lhés desejo um bom dia")
+
+print(5+3)
+print(5-2)
+print(5*3)
+print(10/2)
+print(3**3)
+
+idade = 18
+if idade >= 18:
+    print("Maior de idade")
+else:
+    print("Menor de idade")
+
+for i in range(5):
+    print("Número:", i)
+
+    
